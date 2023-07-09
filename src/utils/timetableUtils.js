e9e7219ec87c205a7fd7f7583dd9c7e427b96e93
rajostelly <110835737+rajostelly@users.noreply.github.com>
2023-07-10 01:18:21 +0200

    Add files via upload

--- src/utils/timetableUtils.js
+++ src/utils/timetableUtils.js
@@ -1,46 +1,73 @@
 export function generateTimetable(subjects) {
-    const matrice = [
-      ["", "", "", ""],
-      ["", "", "", ""],
-      ["", "", "", ""],
-      ["", "", "", ""],
-      ["", "", "", ""],
-      ["", "", "", ""]
-    ];
-  
-    const availableSubjects = [...subjects];
-  
-    for (let i = 0; i < matrice.length; i++) {
-      let row = matrice[i];
-      let j = 0;
-  
-      while (j < row.length && availableSubjects.length > 0) {
-        const randomIndex = Math.floor(Math.random() * availableSubjects.length);
-        const subject = availableSubjects[randomIndex];
-  
-        const { duration, value } = subject;
-  
-        if (duration === 2 && row[j] === "") {
-          row[j] = value;
-          availableSubjects.splice(randomIndex, 1);
-          j++;
-        } else if (duration === 4 && row[j] === "" && row[j + 1] === "") {
-          row[j] = value;
-          row[j + 1] = value;
-          availableSubjects.splice(randomIndex, 1);
-          j += 2;
-        } else if (duration === 6 && row[j] === "" && row[j + 1] === "" && row[j + 2] === "") {
-          row[j] = value;
-          row[j + 1] = value;
-          row[j + 2] = value;
-          availableSubjects.splice(randomIndex, 1);
-          j += 3;
-        } else {
-          j++;
-        }
+  const matrice = [
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""]
+  ];
+
+  const availableSubjects = [...subjects];
+  const maxAttempts = 2000; // Limite de tentatives
+
+  // Insérer au moins une valeur dans chaque ligne
+  for (let i = 0; i < matrice.length; i++) {
+    const row = matrice[i];
+
+    if (availableSubjects.length > 0) {
+      const randomIndex = Math.floor(Math.random() * availableSubjects.length);
+      const subject = availableSubjects[randomIndex];
+
+      const { duration, value } = subject;
+
+      if (duration === 2) {
+        row[0] = value;
+        availableSubjects.splice(randomIndex, 1);
+      } else if (duration === 4 && row.length >= 2) {
+        row[0] = value;
+        row[1] = value;
+        availableSubjects.splice(randomIndex, 1);
+      } else if (duration === 6 && row.length >= 3) {
+        row[0] = value;
+        row[1] = value;
+        row[2] = value;
+        availableSubjects.splice(randomIndex, 1);
       }
     }
-  
-    return matrice;
   }
-  
+
+  // Réorganiser la matrice finale en utilisant l'algorithme de Fisher-Yates avec vérification des répétitions
+  for (let i = 0; i < matrice.length; i++) {
+    const row = matrice[i];
+    let attempts = 0;
+
+    while (hasDuplicates(row) && attempts < maxAttempts) {
+      for (let j = row.length - 1; j > 0; j--) {
+        const randomIndex = Math.floor(Math.random() * (j + 1));
+        [row[j], row[randomIndex]] = [row[randomIndex], row[j]];
+      }
+
+      attempts++;
+    }
+  }
+
+  return matrice;
+}
+
+// Vérifier les répétitions dans une ligne
+function hasDuplicates(array) {
+  const values = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+
+    if (values.has(value)) {
+      return true;
+    }
+
+    values.add(value);
+  }
+
+  return false;
+}
